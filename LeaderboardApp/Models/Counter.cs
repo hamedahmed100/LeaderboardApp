37a3bf278563a120e6fc1f1fb9dd65e93333ad37
Hamed Ahmed <hamedahmedhamed100@gmail.com>
2024-10-28 19:16:33 +0100

    Create counter model

---
+++ LeaderboardApp/Models/Counter.cs
@@ -0,0 +1,10 @@
+﻿namespace LeaderboardApp.Models
+{
+    public class Counter
+    {
+        public int CounterId { get; set; }
+        public int UserId { get; set; }
+        public int Steps { get; set; }
+        public int TeamId { get; set; }
+    }
+}
